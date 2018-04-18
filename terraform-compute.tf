d4dcaf2b3e5495f788f3c9e5c9928d4c538cc324
Stuart Moore <stuart.moore@gmail.com>
2018-04-18 19:21:27 +0100

    Add disk

--- terraform-compute.tf
+++ terraform-compute.tf
@@ -1,3 +1,10 @@
+resource "google_compute_disk" "github" {
+  name = "github"
+  type = "pd-ssd"
+  zone = "${lookup(var.region_params["${var.region}"],"zone1")}"
+  size = "${var.github_disk}"
+}
+
 resource "google_compute_instance" "github" {
   name         = "github"
   machine_type = "${var.github_machine_type}"
@@ -8,6 +15,10 @@ resource "google_compute_instance" "github" {
     initialize_params {
       image = "github-enterprise-public/github-enterprise-${var.github_version}"
     }
+  }
+
+  attached_disk {
+    source = "${google_compute_disk.github.self_link}"
   }
 
   network_interface {
